8c39391cf0ae8db0dd79f310adf16ea1a4ce2460
desperado1802 <mumun_ali@hotmail.com>
2023-04-22 21:11:56 +0300

    added rotateY to the audi cards, also added pulsating animation to the playing card

--- src/components/Frequencies.jsx
+++ src/components/Frequencies.jsx
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import audioData from "../audioData";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Frequencies() {
+  const [isHovering, setIsHovering] = useState(audioData.map(() => false));
+  const [pauseRotation, setPauseRotation] = useState(
+    audioData.map(() => false)
+  );
+
+  const handleHover = (idx) => {
+    if (pauseRotation[idx]) return;
+    const newHoverStates = [...isHovering];
+    newHoverStates[idx] = !newHoverStates[idx];
+    setIsHovering(newHoverStates);
+  };
+
+  const playingFrequencyTrue = (idx) => {
+    const newPlaying = [...pauseRotation];
+    newPlaying[idx] = true;
+    setPauseRotation(newPlaying);
+  };
+  const playingFrequencyFalse = (idx) => {
+    const newPlaying = [...pauseRotation];
+    newPlaying[idx] = false;
+    setPauseRotation(newPlaying);
+  };
+
   return (
-    <div className="frequenciesMainContainer">
+    <div>
       {audioData.map((item, idx) => (
-        <div key={idx} className="frequenciesWrapper">
-          <h3>{item.title}</h3>
-          <p>{item.text}</p>
-          <ReactAudioPlayer
-            className="audioPlayer"
-            src={item.audio}
-            loop
-            controls
-          />
+        <div
+          key={idx}
+          className={`hover-div ${isHovering[idx] ? "hover-div--hovered" : ""}`}
+          onMouseEnter={() => handleHover(idx)}
+          onMouseLeave={() => handleHover(idx)}
+        >
+          <div key={idx} className="frequenciesWrapperFront">
+            <h3>{item.title}</h3>
+            <ReactAudioPlayer
+              className="audioPlayer"
+              src={item.audio}
+              loop
+              controls
+            />
+          </div>
+          <div
+            className={`frequenciesWrapperBack ${
+              pauseRotation[idx] ? "pulsate" : ""
+            }`}
+          >
+            <p>{item.text}</p>
+            <ReactAudioPlayer
+              className="audioPlayer"
+              src={item.audio}
+              onPlay={() => playingFrequencyTrue(idx)}
+              onPause={() => playingFrequencyFalse(idx)}
+              loop
+              controls
+            />
+          </div>
         </div>
       ))}
     </div>

136d7f28c9ed26788654fe71186f031f20c5b3da
desperado1802 <mumun_ali@hotmail.com>
2023-04-23 21:34:03 +0300

    removed the hover transform effect as it was uneccesary and making the project complicated for the user

--- src/components/Frequencies.jsx
+++ src/components/Frequencies.jsx
@@ -3,17 +3,9 @@ import audioData from "../audioData";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Frequencies() {
-  const [isHovering, setIsHovering] = useState(audioData.map(() => false));
   const [pauseRotation, setPauseRotation] = useState(
     audioData.map(() => false)
   );
-
-  const handleHover = (idx) => {
-    if (pauseRotation[idx]) return;
-    const newHoverStates = [...isHovering];
-    newHoverStates[idx] = !newHoverStates[idx];
-    setIsHovering(newHoverStates);
-  };
 
   const playingFrequencyTrue = (idx) => {
     const newPlaying = [...pauseRotation];
@@ -27,38 +19,24 @@ export default function Frequencies() {
   };
 
   return (
-    <div>
+    <div className="hover-div">
       {audioData.map((item, idx) => (
         <div
           key={idx}
-          className={`hover-div ${isHovering[idx] ? "hover-div--hovered" : ""}`}
-          onMouseEnter={() => handleHover(idx)}
-          onMouseLeave={() => handleHover(idx)}
+          className={`frequenciesWrapper ${
+            pauseRotation[idx] ? "pulsate" : ""
+          }`}
         >
-          <div key={idx} className="frequenciesWrapperFront">
-            <h3>{item.title}</h3>
-            <ReactAudioPlayer
-              className="audioPlayer"
-              src={item.audio}
-              loop
-              controls
-            />
-          </div>
-          <div
-            className={`frequenciesWrapperBack ${
-              pauseRotation[idx] ? "pulsate" : ""
-            }`}
-          >
-            <p>{item.text}</p>
-            <ReactAudioPlayer
-              className="audioPlayer"
-              src={item.audio}
-              onPlay={() => playingFrequencyTrue(idx)}
-              onPause={() => playingFrequencyFalse(idx)}
-              loop
-              controls
-            />
-          </div>
+          <h3>{item.title}</h3>
+          <p>{item.text}</p>
+          <ReactAudioPlayer
+            className="audioPlayer"
+            src={item.audio}
+            onPlay={() => playingFrequencyTrue(idx)}
+            onPause={() => playingFrequencyFalse(idx)}
+            loop
+            controls
+          />
         </div>
       ))}
     </div>
